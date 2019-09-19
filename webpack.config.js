b3942fe953aae5dc55e30e4a5d94429716f4403d
pulabio <pulabio@gmail.com>
2019-09-19 16:49:45 -0300

    added  css-loader and style-loader as devDependencies and configured them

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,15 @@ module.exports = {
         use:{
           loader: 'babel-loader'
         }
+      }, {
+        test: /\.css/,
+        use: [
+          {loader: 'style-loader'},
+          {loader: 'css-loader'},
+        ]
       }
+
+
     ]
   }
 };
