500610351847dab0405c7e4bc4f00ae89ec33f6a
pulabio <pulabio@gmail.com>
2019-09-19 08:50:00 -0300

    chore: add babel config

---
+++ babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  presets: [
+    "@babel/preset-env", //cuida das funcionalidades do javascript que o navegador não entende
+    "@babel/preset-react" // cuida das funcionalidades específicas do React (como JSX)
+  ],
+};
