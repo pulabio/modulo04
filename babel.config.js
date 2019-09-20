6a527d5aaff891fa49a0aa2f7017530a89680c16
pulabio <pulabio@gmail.com>
2019-09-20 16:49:48 -0300

    chore: add and configure @babel/plugin-proposal-class-properties
    This allows us to use state in a component without setting a constructor explicitly

--- babel.config.js
+++ babel.config.js
@@ -3,4 +3,7 @@ module.exports = {
     "@babel/preset-env", //cuida das funcionalidades do javascript que o navegador não entende
     "@babel/preset-react" // cuida das funcionalidades específicas do React (como JSX)
   ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties'
+  ]
 };
